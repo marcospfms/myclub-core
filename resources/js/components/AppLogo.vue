cb4dd5ee39033aaa0945f80cc736d83354fbb5c3
Marcos Mamede <marcospaulo.20@gmail.com>
2026-04-09 14:14:31 -0400

    Feat: logo dentro do sistema

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,14 +1,11 @@
-<script setup lang="ts">
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
-</script>
-
 <template>
-    <div
-        class="flex aspect-square size-8 items-center justify-center rounded-md bg-primary text-primary-foreground"
-    >
-        <AppLogoIcon class="size-5 fill-current text-white" />
-    </div>
+    <img
+        src="/brand/symbol.svg"
+        alt="MyClub"
+        class="size-13"
+    />
     <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate leading-tight font-semibold">MyClub</span>
+        <span class="block text-xs text-muted-foreground">Administração</span>
     </div>
 </template>
